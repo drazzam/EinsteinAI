827fddb5c6b34a0e1a55ba3d4dcbc12dd6bf0061
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-04-22 02:03:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,18 +27,29 @@ def save_cover_letter_to_docx(cover_letter, filename):
     doc.add_paragraph(cover_letter)
     doc.save(filename)
 
+# Custom CSS to handle the logo switch based on theme
+custom_css = r"""
+<style>
+    body.light-mode .sidebar-content {
+        display: none;
+    }
+    body.dark-mode .sidebar-content {
+        display: block;
+    }
+</style>
+"""
 
+st.write(custom_css, unsafe_allow_html=True)
+
+    
 # Streamlit app
 st.set_page_config(page_title="EinsteinAI", layout="wide", initial_sidebar_state="expanded")
 
 # Add logo to sidebar
 
 # Add logo to sidebar based on theme
-theme = st.get_option("theme.primaryColor")
-if theme == "black":
-    st.sidebar.image('https://raw.githubusercontent.com/drazzam/EinsteinAI/main/white_logo.png')
-else:
-    st.sidebar.image('https://raw.githubusercontent.com/drazzam/EinsteinAI/main/logo.png')
+st.sidebar.markdown('<div class="sidebar-content"><img src="https://raw.githubusercontent.com/drazzam/EinsteinAI/main/logo.png" alt="Logo"></div>', unsafe_allow_html=True)
+st.sidebar.markdown('<div class="sidebar-content" style="display: none;"><img src="https://raw.githubusercontent.com/drazzam/EinsteinAI/main/white_logo.png" alt="Logo"></div>', unsafe_allow_html=True)
 
 st.sidebar.markdown("##### Developed by Cerebrovascular Research Lab at Albert Einstein College of Medicine")
 
