8487b122a62c23e7d1c1490994f7613676243b72
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-04-22 02:50:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,6 @@ if api_key:
 st.sidebar.markdown("### About")
 if st.sidebar.button("Show About"):
     st.sidebar.markdown(
-        "This Application Was Developed By:  \n Ahmed Y. Azzam, MD, MEng  \n Muhammed Amir Essibayi, MD  \n Under Supervision of Cerebrovascular Research Lab Principal Investigator:  \n David Altschul, MD"
+        "This Application Was Developed By:  \n  \n Ahmed Y. Azzam, MD, MEng  \n Muhammed Amir Essibayi, MD  \n  \n Under Supervision of Cerebrovascular Research Lab Principal Investigator:  \n  \n David Altschul, MD"
     )
 
