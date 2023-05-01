42bfa85e00b5c6589a52fb8f1c04456088f3f6ce
Ahmed Y. Azzam <109034951+drazzam@users.noreply.github.com>
2023-05-01 06:28:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from streamlit import components
 import docx
 import io
 import base64
@@ -20,6 +21,21 @@ st.sidebar.image('https://raw.githubusercontent.com/drazzam/EinsteinAI/main/logo
 
 st.sidebar.markdown("##### Developed by Cerebrovascular Research Lab at Albert Einstein College of Medicine")
 
+def copy_text_to_clipboard(text):
+    b64_text = base64.b64encode(text.encode()).decode()
+    components.v1.html(f'''<textarea id="text_to_copy" style="opacity:0;">{text}</textarea>
+        <button onclick="copyTextToClipboard()">Copy Generated Prompt to Clipboard</button>
+        <script>
+        function copyTextToClipboard() {{
+            var copyText = document.getElementById("text_to_copy");
+            copyText.select();
+            copyText.setSelectionRange(0, 99999);
+            document.execCommand("copy");
+        }}
+        </script>''', height=60)
+
+st.set_page_config(cache_clear_on_refresh=True, initial_sidebar_state="auto")
+
 tool_selection = st.sidebar.radio("Select a tool:", [
     "Risk of Bias Assessment Tool",
     "Cover Letter Generator",
@@ -34,39 +50,32 @@ tool_selection = st.sidebar.radio("Select a tool:", [
 ])
 
 if tool_selection == "Research Advisor Tool":
-    st.title("Research Advisor Tool")
-    st.write("Optimized For ChatGPT (GPT-4)")
+  st.title("Research Advisor Tool")
+  st.write("Optimized For ChatGPT (GPT-4)")
 
-    # Input fields
-    manuscript_title = st.text_input("Manuscript Title:")
-    research_paper_type = st.text_input("Type of Research Paper: (e.g. systematic review, meta-analysis, review article, retrospective study)")
-    section_to_criticize = st.text_input("The Section Within The Draft To Be Criticized: (e.g. introduction, methods, results, discussion)")
-    section = st.text_area("Paste The Section From Manuscript Here:")
+  # Input fields
+  manuscript_title = st.text_input("Manuscript Title:")
+  research_paper_type = st.text_input("Type of Research Paper: (e.g. systematic review, meta-analysis, review article, retrospective study)")
+  section_to_criticize = st.text_input("The Section Within The Draft To Be Criticized: (e.g. introduction, methods, results, discussion)")
+  section = st.text_area("Paste The Section From Manuscript Here:")
 
-    # Button to generate the prompt
-    generate_button = st.button("Generate Prompt")
+  # Button to generate the prompt
+  generate_button = st.button("Generate Prompt")
 
-    # Function to create the prompt
-    def create_prompt(manuscript_title, research_paper_type, section_to_criticize):
-        prompt = f'''This a draft manuscript proposed for a research paper entitled "{manuscript_title}" which is a {research_paper_type} article. Could you criticize and review the following {section_to_criticize} section in the manuscript draft, and provide a detailed feedback report on how to improve it from all aspects including: appropriate writing and proofreading, appropriate methodology and sequence, and the science within the section itself. Generate a comprehensive professional report and recommendations for this manuscript, please! That's the {section_to_criticize} section: {section}'''
-        return prompt
+  # Function to create the prompt
+  def create_prompt(manuscript_title, research_paper_type, section_to_criticize):
+      prompt = f'''This a draft manuscript proposed for a research paper entitled "{manuscript_title}" which is a {research_paper_type} article. Could you criticize and review the following {section_to_criticize} section in the manuscript draft, and provide a detailed feedback report on how to improve it from all aspects including: appropriate writing and proofreading, appropriate methodology and sequence, and the science within the section itself. Generate a comprehensive professional report and recommendations for this manuscript, please! That's the {section_to_criticize} section: {section}'''
+      return prompt
 
-    if generate_button:
-        if manuscript_title and research_paper_type and section_to_criticize and section:
-            # Generate the prompt
-            prompt = create_prompt(manuscript_title, research_paper_type, section_to_criticize)
-            # Display the prompt in a textbox and add a button to copy its content
-            prompt_textbox = st.text_area("Generated Prompt:", value=prompt, height=150)
-            copy_button = st.button("Copy Generated Prompt to Clipboard")
-            if copy_button:
-                st.caching.cache.clear_cache()  # To avoid clipboard caching issue
-                st.copy(prompt)  # Copy the text to the clipboard
-                st.success("Generated Prompt copied to clipboard!")
-        else:
-            st.error("Please fill in all the input fields before generating the prompt.")
-        
-    # Configure the app to prevent caching and to reset query parameters after copying
-    st.set_page_config(cache_clear_on_refresh=True, initial_sidebar_state="auto")
+  if generate_button:
+      if manuscript_title and research_paper_type and section_to_criticize and section:
+          # Generate the prompt
+          prompt = create_prompt(manuscript_title, research_paper_type, section_to_criticize)
+          # Display the prompt in a textbox and add a button to copy its content
+          prompt_textbox = st.text_area("Generated Prompt:", value=prompt, height=150)
+          copy_text_to_clipboard(prompt)
+      else:
+          st.error("Please fill in all the input fields before generating the prompt.")
 
     
 st.sidebar.markdown("### About")
